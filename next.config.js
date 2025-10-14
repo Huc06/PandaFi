270961a10d8af4b39846df3017597f5a065f13a2
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2025-10-14 23:14:34 +0700

    fix: web3 dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  output: "export",
+  distDir: "dist",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -15,8 +15,20 @@ const nextConfig = {
         net: false,
         tls: false,
         crypto: false,
+        // Fix for Web3 wallet dependencies
+        encoding: false,
+        "pino-pretty": false,
+        "@react-native-async-storage/async-storage": false,
       };
     }
+
+    // Ignore optional dependencies warnings
+    config.ignoreWarnings = [
+      { module: /node-fetch/ },
+      { module: /pino/ },
+      { module: /@metamask\/sdk/ },
+    ];
+
     return config;
   },
 };
